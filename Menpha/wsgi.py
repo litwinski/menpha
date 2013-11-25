900b0e8c1ff3a641d21cec298bc1e749dce82fbd
seanmavley <seanmavley@gmail.com>
2013-11-25 18:26:56 +0000

    updated wsgi for heroku

--- Menpha/wsgi.py
+++ Menpha/wsgi.py
@@ -31,7 +31,7 @@ application = get_wsgi_application()
 #from Menpha.wsgi import HelloWorldApplication
 #application = HelloWorldApplication(application)
 
-#from django.core.wsgi import get_wsgi_application
-#from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
