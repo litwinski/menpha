a822d4f9a291d0899f2ead616415d42ac6e50766
seanmavley <seanmavley@gmail.com>
2013-12-10 14:06:51 +0000

    updates

--- main/forms.py
+++ main/forms.py
@@ -7,7 +7,11 @@ from main.models import Item
 class ItemForm(ModelForm):
 	class Meta:
 		model = Item
-		fields = ['device', 'slug', 'description', 'photo', 'stolen']
+		fields = ('device', 'slug', 'description', 'photo', 'stolen')
+		labels = {
+			'device': 'Name of my device',
+			'slug': 'IMEI of my device'
+		}
 		
 	#device = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Device name'}))
 	#imei = forms.CharField(max_length=15, 
@@ -19,3 +23,19 @@ class ItemForm(ModelForm):
 	#			'rows':4,
 	#			'maxlength': 350,
 	#	}))
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ('name', 'title', 'birth_date')
+        labels = {
+            'name': _('Writer'),
+        }
+        help_texts = {
+            'name': _('Some useful help text.'),
+        }
+        error_messages = {
+            'name': {
+                'max_length': _("This writer's name is too long."),
+            },
+        }﻿
