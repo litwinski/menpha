f5743262f200d6515f1c80463b92925a2768762f
seanmavley <seanmavley@gmail.com>
2013-12-19 02:04:48 +0000

    settings

--- Menpha/settings.py
+++ Menpha/settings.py
@@ -4,7 +4,7 @@
 
 #import bpy
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -205,3 +205,22 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'menphaproject@gmail.com'
 EMAIL_HOST_PASSWORD = '00113399'
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
