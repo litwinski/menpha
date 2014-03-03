b134c243be2494b32cc16e5d79c8e62aebbe87bf
Nkansah Rexford <seanmavley@gmail.com>
2014-03-03 12:51:26 +0200

    push

--- Menpha/settings.py
+++ Menpha/settings.py
@@ -209,28 +209,22 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'your@email.address'
 EMAIL_HOST_PASSWORD = 'yourpassword' # forgotten? Ask NSA
 
-try:
-   from local_settings import *
-except ImportError, e:
-   pass
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-if not DEBUG:
-    # Parse database configuration from $DATABASE_URL
-    import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-    # Allow all host headers
-    ALLOWED_HOSTS = ['localhost']
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-    # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
